3e4c3603dc830d174955143f2ea01cb44a81cb5e
Harry <gelo.soer@gmail.com>
2020-09-01 22:08:10 +0800

    root: Added a proper classname integration and transitions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,25 @@
 import Header from 'components/Header';
 import { useRouter } from 'next/router'
+import cn from 'classnames'
 import 'styles/globals.css';
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
   const isDarkBg = ['/', '/home'].includes(router.pathname)
-  const bgColor = isDarkBg ? 'bg-black' : 'bg-white'
+  const bgColor = isDarkBg ? 'bg-black text-white' : 'bg-white text-dark'
+  const textColor = isDarkBg ? 'text-white' : 'text-dark'
 
   return (
-    <div className={`min-h-screen ${bgColor}`}>
+    <div
+      className={cn([
+        'min-h-screen',
+        'transition-colors',
+        'duration-1000',
+        'ease-in',
+        bgColor,
+        textColor
+      ])}
+    >
       {router.pathname !== '/'
         ? <Header isDarkBg={isDarkBg} />
         : null
